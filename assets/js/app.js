9ee370effd6bd46d53ba603c1dfea2375796bc2b
shanja-glinka <rul3iofu1s7@outlook.com>
2023-03-07 19:58:28 +0300

    Ready front

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@
  * @param {string} request.url
  * @param {string} request.method
  * @param {function} request.callback
+ * @param {function} request.onerror
  * @param {mixed} request.data
  * @param {boolean} request.json
  */
@@ -12,6 +13,7 @@ const ajax = (request) => {
     let url = request.url;
     let method = typeof request.method === 'undefined' ? 'GET' : request.method;
     let callback = typeof request.callback !== 'function' ? null : request.callback;
+    let onerror = typeof request.onerror !== 'function' ? null : request.onerror;
     let data = typeof request.data === 'undefined' ? null : request.data;
     let json = typeof request.json !== 'boolean' ? false : request.json;
 
@@ -19,15 +21,11 @@ const ajax = (request) => {
         throw ('URl required');
 
     if (data && typeof (data) === 'object') {
-        if (json === true)
-            data = JSON.stringify(data);
-        else {
-            let tempdata = '', e = encodeURIComponent;
-            for (k in data) {
-                tempdata += '&' + e(k) + '=' + e(data[k]);
-            }
-            data = tempdata.slice(1);
+        let tempdata = '', e = encodeURIComponent;
+        for (k in data) {
+            tempdata += '&' + e(k) + '=' + e(data[k]);
         }
+        data = tempdata.slice(1);
     }
 
     try {
@@ -38,15 +36,386 @@ const ajax = (request) => {
         xhr.setRequestHeader('Content-type', (json !== true ? 'application/x-www-form-urlencoded' : 'application/json'));
 
         xhr.onreadystatechange = () => {
-            if (xhr.readyState > 3 && callback) {
-                callback((json === true ? JSON.parse(xhr.responseText) : xhr.responseText), xhr);
+            try {
+                if (xhr.readyState > 3 && callback) {
+                    callback((json === true ? JSON.parse(xhr.responseText) : xhr.responseText), xhr);
+                }
+            } catch (e) {
+                if (onerror)
+                    onerror(e, xhr.responseText);
+                console.log(e);
             }
         };
         xhr.send(data)
     } catch (e) {
+        if (onerror)
+            onerror(e);
         console.log(e);
     }
 };
+
+
+
+
+
+
+
+class ItemAction {
+
+    setProcess(text) {
+        document.querySelector('.object-tree-process p').innerText = text;
+    }
+
+    getElementByData(data, id = null) {
+        return document.querySelector(id === null ? '[' + data + ']' : '[' + data + '="' + id + '"]');
+    }
+
+    getDescrElement(id) {
+        return this.getElementByData('data-descr-id', id);
+    }
+
+}
+
+
+class RenameItem extends ItemAction {
+    createRenameActions(id) {
+        let htmlWorker = new HtmlWorker();
+
+        let actions = htmlWorker.createElement('div', { 'class': 'group-title group-actions' });
+
+        let save = htmlWorker.createElement('i', { 'class': 'icon icon-check' });
+        let cancel = htmlWorker.createElement('i', { 'class': 'icon icon-close' });
+
+        save.addEventListener('click', () => {
+            this.save(id);
+        });
+
+        cancel.addEventListener('click', () => {
+            this.cancel(id);
+        });
+
+
+        actions.appendChild(save);
+        actions.appendChild(cancel);
+
+        return actions;
+    }
+
+    rename(id) {
+        new TreeActions().descrToggle(id);
+
+        let descr = this.getDescrElement(id);
+
+        if (descr.querySelector('input'))
+            return;
+
+
+        let htmlWorker = new HtmlWorker();
+
+        let titleElement = descr.querySelector('h2');
+        let descrElement = descr.querySelector('p');
+
+        let tileText = titleElement.innerText;
+        let descrText = descrElement.innerText;
+
+
+        descr.classList.add('input-view')
+
+
+        let input = htmlWorker.createElement('input', {
+            'type': 'text',
+            'value': tileText
+        });
+        let tarea = htmlWorker.createElement('textarea', {
+            'type': 'text',
+            'value': descrText,
+            'innerText': descrText
+        });
+
+        titleElement.appendChild(input);
+        descrElement.appendChild(tarea);
+
+        descr.appendChild(this.createRenameActions(id));
+    }
+
+    save(id) {
+        this.setProcess('processed...');
+
+        ajax({
+            url: defaultRoutes.renameGroupSave + '/' + id,
+            data: {
+                'title': this.getDescrElement(id).querySelector('input').value,
+                'descr': this.getDescrElement(id).querySelector('textarea').value,
+            },
+            json: true,
+            method: 'PUT',
+            callback: (responce) => {
+                this.saveSuccess(id, responce);
+            },
+            onerror: (responce) => {
+                this.saveError(id, responce);
+            }
+        });
+
+        // IGNORE_AJAX
+        this.saveSuccess(id, {
+            "id": 8,
+            // "parent": 1,
+            "title": this.getDescrElement(id).querySelector('input').value,
+            "descr": this.getDescrElement(id).querySelector('textarea').value,
+        });
+    }
+
+    saveSuccess(id, responce) {
+        this.setProcess('item - ' + id + ' - saved successful');
+        let descr = this.getDescrElement(id);
+
+        descr.querySelector('h2').innerHTML = responce.title;
+        descr.querySelector('p').innerHTML = responce.descr;
+
+        document.querySelector('[data-group-id="' + id + '"] span').innerText = responce.title;
+
+        this.cancel(id);
+    }
+
+    saveError(id, responce) {
+        this.setProcess('item - ' + id + ' - save failed');
+    }
+
+
+    cancel(id) {
+        let descr = this.getDescrElement(id);
+
+        if (descr.querySelector('.group-actions'))
+            descr.removeChild(descr.querySelector('.group-actions'));
+
+        descr.classList.remove('input-view');
+
+        new TreeActions().descrToggle(id);
+    }
+}
+
+
+class RemoveItem extends ItemAction {
+
+    remove(id) {
+
+        if (!window.confirm('Do you really want to delete the structure?'))
+            return;
+
+
+        this.setProcess('processed...');
+
+        ajax({
+            url: defaultRoutes.removeGroupSave + '/' + id,
+            json: true,
+            method: 'DELETE',
+            callback: (responce) => {
+                this.removeSuccess(id);
+            },
+            onerror: (responce) => {
+                this.removeError(id, responce);
+            }
+        });
+
+        this.removeSuccess(id);
+    }
+
+    removeSuccess(id) {
+        this.setProcess('item - ' + id + ' - removed successful');
+
+        let item = this.getElementByData('data-group-id', id);
+
+        item.parentElement.removeChild(item);
+
+        new ItemDescr().removeDescr(id);
+    }
+
+    removeError(id, responce) {
+        this.setProcess('item - ' + id + ' - remove failed');
+    }
+}
+
+
+class AddItem extends ItemAction {
+
+    createActions() {
+        let htmlWorker = new HtmlWorker();
+
+        let actions = htmlWorker.createElement('div', { 'class': 'group-title group-actions' });
+
+        let save = htmlWorker.createElement('i', { 'class': 'icon icon-check' });
+        let cancel = htmlWorker.createElement('i', { 'class': 'icon icon-close' });
+
+        save.addEventListener('click', () => {
+            this.saveItem();
+        });
+
+        cancel.addEventListener('click', () => {
+            this.cancel();
+        });
+
+
+        actions.appendChild(save);
+        actions.appendChild(cancel);
+
+        return actions;
+    }
+
+    getItemParent() {
+        return this.getElementByData('data-parent-add').getAttribute('data-parent-add');
+    }
+
+    createDescr() {
+        new ItemDescr().innerDescr(-1, 'Title', 'Description');
+
+        let htmlWorker = new HtmlWorker();
+
+        let descr = this.getDescrElement(-1);
+
+        let titleElement = descr.querySelector('h2');
+        let descrElement = descr.querySelector('p');
+
+
+        descr.classList.add('input-view');
+
+
+        let input = htmlWorker.createElement('input', {
+            'type': 'text',
+            'value': titleElement.innerText
+        });
+        let tarea = htmlWorker.createElement('textarea', {
+            'type': 'text',
+            'value': descrElement.innerText,
+            'innerText': descrElement.innerText
+        });
+
+        titleElement.appendChild(input);
+        descrElement.appendChild(tarea);
+
+        descr.appendChild(this.createActions());
+
+        return descr;
+    }
+
+
+    add(parentId) {
+
+        let descr = this.getDescrElement(-1);
+
+        if (!descr)
+            descr = this.createDescr();
+
+        descr.setAttribute('data-parent-add', parentId);
+
+        new TreeActions().descrToggle(-1);
+    }
+
+
+
+    saveItem() {
+        this.setProcess('process...');
+
+        ajax({
+            url: defaultRoutes.addGroup,
+            json: true,
+            data: {
+                'parent': this.getItemParent(),
+                'title': this.getDescrElement(-1).querySelector('input').value,
+                'descr': this.getDescrElement(-1).querySelector('textarea').value
+            },
+            method: 'POST',
+            callback: (responce) => {
+                this.saveItemSuccess(responce);
+            },
+            onerror: (responce) => {
+                this.saveItemError(responce);
+            }
+        });
+
+        // IGNORE_AJAX
+        this.saveItemSuccess({
+            "id": 8,
+            "parent": this.getItemParent(),
+            "title": this.getDescrElement(-1).querySelector('input').value,
+            "descr": this.getDescrElement(-1).querySelector('textarea').value,
+        });
+    }
+
+
+    instalItem(data, actionButtons) {
+        return new StructItems({
+            id: data.id,
+            title: data.title,
+            descr: data.descr,
+            actionButtons: actionButtons
+        });
+    }
+
+    itemInsert(parent, data, actionButtons) {
+        let item = this.instalItem(data, actionButtons).create();
+
+
+        parent.insertBefore(item, this.getElementByData('data-parent-id', data.parent));
+    }
+
+
+    saveItemSuccess(responce) {
+        this.setProcess('new item was created');
+
+        if (typeof responce.parent === 'undefined')
+            responce.parent = -1;
+
+        let admin = typeof adminView === 'boolean' ? adminView : false;
+
+        if (responce.parent === -1) {
+            this.itemInsert(document.querySelector('.group-tree-container'), responce, admin);
+
+        } else {
+            let parentElement = this.getElementByData('data-group-id', responce.parent);
+            let tinsertTo = null;
+
+            parentElement.childNodes.forEach(el => {
+                if (el.tagName.toLowerCase() === 'ul' && el.classList.contains('sub-group')) {
+                    tinsertTo = el;
+                    return;
+                }
+            });
+
+            if (tinsertTo !== null) {
+
+                this.itemInsert(tinsertTo, responce, admin);
+
+                !tinsertTo.classList.contains('active') ? tinsertTo.classList.add('active') : null;
+            } else {
+                let treestruct = new TreeStruct(responce, admin);
+
+                parentElement.appendChild(treestruct.createSubStruct(this.instalItem(responce, admin), null));
+            }
+
+            this.cancel();
+        }
+
+    }
+
+    saveItemError(responce) {
+        this.setProcess('item was not saved');
+    }
+
+    cancel() {
+        let descr = this.getDescrElement(-1);
+
+        descr.removeAttribute('data-parent-add');
+
+        new TreeActions().descrToggle(-1);
+    }
+}
+
+
+
+
+
+
 
 
 
@@ -58,11 +427,15 @@ class TreeActions {
 
 
     rename(renameId) {
-        console.log(renameId);
-
+        new RenameItem().rename(renameId);
     }
+
     remove(removeId) {
-        console.log(removeId);
+        new RemoveItem().remove(removeId);
+    }
+
+    addStruct(parentId) {
+        new AddItem().add(parentId);
     }
 
     descrToggle(descrId) {
@@ -137,6 +510,35 @@ class ItemDescr extends HtmlWorker {
 
         this.getDescrContainer().appendChild(item);
     }
+
+    removeDescr(id) {
+        let item = this.getDescrContainer().querySelector('[data-descr-id="' + id + '"]');
+        item.parentElement.removeChild(item);
+    }
+}
+
+
+class StructItemAdd extends HtmlWorker {
+
+    create(parentId) {
+        let element = this.createElement('li', {
+            'class': 'group-item-add',
+            'data-parent-id': parentId
+        });
+
+        let groupTitle = this.createElement('div', { 'class': 'group-title' });
+
+        groupTitle.appendChild(this.createElement('i', { 'class': 'icon icon-plus' }));
+        groupTitle.appendChild(this.createElement('span', { 'innerText': 'Add new item' }));
+
+        groupTitle.addEventListener('click', () => {
+            new TreeActions().addStruct(parentId);
+        });
+
+        element.appendChild(groupTitle);
+
+        return element;
+    }
 }
 
 
@@ -155,7 +557,7 @@ class StructItems extends HtmlWorker {
     innerDescr(call) {
         new ItemDescr().innerDescr(this.id, this.title, this.descr);
 
-        call.addEventListener('click', () => {
+        call.addEventListener('click', (e) => {
             new TreeActions().descrToggle(this.id);
         });
     }
@@ -211,13 +613,12 @@ class StructItems extends HtmlWorker {
 
         let groupTitle = this.createElement('div', { 'class': 'group-title' });
 
-
-        groupTitle.appendChild(this.createElement('i', { 'class': 'icon icon-default' }));
         groupTitle.appendChild(this.createElement('span', { 'innerText': this.title }));
 
 
         if (dropDown === true)
             groupTitle.appendChild(this.createDropDown());
+
 
         if (this.actionButtons === true)
             groupTitle.appendChild(this.createActionButtons());
@@ -240,46 +641,76 @@ class TreeStruct extends HtmlWorker {
         this.actionButtons = actionButtons;
     }
 
-    createSubStruct(item, data) {
+    createAddSubgroup(id) {
+        let ul = this.createElement('ul', { 'class': 'sub-group' });
+        ul.appendChild(new StructItemAdd().create(id));
+        return ul;
+    }
 
-        item = item.create(true);
+    createSubStruct(data = null) {
 
         if (!Array.isArray(this.struct.data))
-            return item;
+            return;
 
+        let parent = -1;
         let ul = this.createElement('ul', { 'class': 'sub-group' });
 
-        data.forEach(el => {
-            let it = new StructItems({
-                id: el.id,
-                title: el.title,
-                descr: el.descr,
-                actionButtons: this.actionButtons
+        if (data !== null)
+            data.forEach(el => {
+                let dropDown = typeof el.data !== 'undefined';
+
+                let it = new StructItems({
+                    id: el.id,
+                    title: el.title,
+                    descr: el.descr,
+                    actionButtons: this.actionButtons
+                }).create(dropDown || this.actionButtons);
+
+                if (dropDown)
+                    it.appendChild(this.createSubStruct(el.data));
+
+
+                if (typeof el.parent !== 'undefined')
+                    parent = el.parent
+
+
+                if (this.actionButtons === true) {
+                    it.appendChild(this.createAddSubgroup(el.id));
+                }
+
+
+
+                ul.appendChild(it);
             });
 
-            ul.appendChild(typeof el.data === 'undefined' ? it.create() : this.createSubStruct(it, el.data));
-        });
+        ul.appendChild(new StructItemAdd().create(parent));
 
-        item.appendChild(ul);
-
-        return item;
+        return ul;
     }
 
 
-
-
     createTree() {
-        let newStruct = this.createElement('ul', { 'class': 'group-tree-container' });;
+        let newStruct = this.createElement('ul', { 'class': 'group-tree-container' });
+
+        let dropDown = typeof this.struct.data !== 'undefined';
 
         let item = new StructItems({
             id: this.struct.id,
             title: this.struct.title,
             descr: this.struct.descr,
             actionButtons: this.actionButtons
-        });
+        }).create(dropDown);
 
 
-        newStruct.appendChild(typeof this.struct.data === 'undefined' ? item.create() : this.createSubStruct(item, this.struct.data));
+        // newStruct.appendChild(typeof this.struct.data === 'undefined' ? item.create() : this.createSubStruct(item, this.struct.data));
+
+        if (dropDown)
+            item.appendChild(this.createSubStruct(this.struct.data, this.actionButtons));
+
+        newStruct.appendChild(item);
+        if (this.actionButtons === true)
+            newStruct.appendChild(new StructItemAdd().create(typeof this.struct.parent !== 'undefined' ? this.struct.parent : -1));
+
 
         return newStruct;
     }
@@ -308,45 +739,33 @@ class TreeRender {
 }
 
 
-class ObjectTree {
-    constructor() {
-        this.objectRender = null;
-    }
-
-    onLoadObjects(respone) {
-        if (!Array.isArray(respone))
-            respone = [respone];
-
-        this.objectRender = new TreeRender({
-            struct: respone,
-            actionButtons: typeof adminView === 'boolean' ? adminView : false
-        });
-
-        this.objectRender.render();
-    }
-
-    load() {
-        if (typeof ajax === 'function')
-            ajax({
-                'url': defaultRoutes.testObjectsTree,
-                'json': true,
-                'callback': (respone) => {
-                    this.onLoadObjects(respone);
-                }
-            });
-    }
-
-}
-
-
 const adminView = true;
 
 const defaultAssetPath = '/assets';
 const defaultRoutes = {
-    'testObjectsTree': defaultAssetPath + '/var/objectsTree-example.json'
+    'testObjectsTree': defaultAssetPath + '/var/objectsTree-example.json',
+    'renameGroupSave': '/struct',
+    'removeGroupSave': '/struct',
+    'addGroup': '/struct'
 }
-const objectTree = new ObjectTree;
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
-    objectTree.load();
+    ajax({
+        'url': defaultRoutes.testObjectsTree,
+        'json': true,
+        'callback': (respone) => {
+
+            if (!Array.isArray(respone))
+                respone = [respone];
+
+            let objectRender = new TreeRender({
+                struct: respone,
+                actionButtons: typeof adminView === 'boolean' ? adminView : false
+            });
+
+            objectRender.render();
+        }
+    });
 });
